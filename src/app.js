e0fcbea090abe482b9e6a47f7dc4794270937d6f
Nilesh <patilnilesh350@gmail.com>
2021-08-29 21:43:15 -0400

    made some updates

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express   = require('express');
 const router    = require('./routes/api');
 const app       = express();
-const port      = 3000;
+const port      = process.env.PORT || 3000;
 
-
+// Routes
 app.use(router);
 
-app.listen(port);
+app.listen(port, () => console.log(`Api is running on port ${port}`));
 
-console.log(`Api is running on port ${port}`)
+
 
 
